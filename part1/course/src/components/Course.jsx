d79433b2617b589a94bd427a468103c5ed5cce50
Federico Villace <federicovillace@gmail.com>
2022-11-19 22:24:18 -0300

    exercise 2.4-2.5

--- part1/course/src/components/Course.jsx
+++ part1/course/src/components/Course.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Content } from "./courseContent/Content";
 import { Header } from "./courseContent/Header";
 
-export const Course = ({ course }) => {
-  const { name, parts } = course;
-
+export const Courses = ({ courses }) => {
   return (
     <div>
-      <Header name={name} />
-      <Content parts={parts} />
+      {courses.map((course) => {
+        return (
+          <div key={course.id}>
+            {" "}
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+          </div>
+        );
+      })}
     </div>
   );
 };
