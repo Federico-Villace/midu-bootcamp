2ffb9a08213363c5397919298620032c9cdac293
Federico Villace <federicovillace@gmail.com>
2022-11-30 19:33:35 -0300

    exercise 2.6

--- part1/phonebook/src/index.js
+++ part1/phonebook/src/index.js
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
+  const [value, setValue] = useState("");
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: value }));
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input type="text" value={value} onChange={handleChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleClick}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((item) => {
+        return <p>{item.name}</p>;
+      })}
     </div>
   );
 };
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
