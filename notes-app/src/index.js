ae2c1e4a72c5ba635d24d6559f129f5ee81f7d74
Federico Villace <federicovillace@gmail.com>
2023-01-21 19:55:44 -0300

    Update index.js

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -3,39 +3,44 @@ import ReactDOM from "react-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { noteReducer } from "./reducers/noteReducer";
 
+const generateId = () => Math.floor(Math.random() * 999999) + 1;
+
 const store = configureStore({ reducer: noteReducer });
 
-store.dispatch({
-  type: "@notes/created",
-  payload: {
-    content: "Testing on Midudevs course",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "@notes/created",
-  payload: {
-    content: "Testing on Midudevs course",
-    important: false,
-    id: 2,
-  },
-});
+const addNote = (e) => {
+  e.preventDefault();
+  const { target } = e;
+  const content = target.note.value;
+  target.note.value = "";
+  store.dispatch({
+    type: "@notes/created",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  });
+};
 
 const App = () => {
   const state = store.getState();
   return (
-    <ul>
-      {state.map((note) => {
-        return (
-          <li key={note.id}>
-            <p>{note.content}</p>
-            <strong>{note.important ? "important" : "not important"}</strong>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button>add note</button>
+      </form>
+      <ul>
+        {state.map((note) => {
+          return (
+            <li key={note.id}>
+              <p>{note.content}</p>
+              <strong>{note.important ? "important" : "not important"}</strong>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
